dbfb816528036e585a283ebcaf58c2e083ec6508
lailai0608 <eileen890608@gmail.com>
2025-12-17 18:13:37 +0800

    fix: final rescue for white screen

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // <--- 檢查有沒有這一行！很重要！
+import './index.css' // 確保 index.css 存在於 src 資料夾下
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
